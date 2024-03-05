93feb91d03a1a0c5afb0760306a685ed5bde5200
Putranugroho14 <putranugroho61@gmail.com>
2024-03-05 12:00:42 +0700

    Add project files.

---
+++ Pertemuan 2 part 3/Pertemuan 2 part 3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int nBilA, nBilB;
+	string status;
+
+	srand(time(0));
+
+	nBilA = rand() % 10;
+	nBilB = rand() % 10;
+
+	if (nBilA == nBilB) {
+		status = "Bilangan A sama besar dengan Bilangan B";
+	}
+	else if (nBilA > nBilB) {
+		status = "Bilangan A lebih besar dari Bilangan B";
+	}
+	else {
+		status = "Bilangan A lebih kecil dari Bilangan B";
+	}
+	cout << "Bilangan A adalah = " << nBilA << endl;
+	cout << "Bilangan B adalah = " << nBilB << endl;
+	cout << "Status Bilangannya adalah = " << status << endl;
+
+	return 0;
+}
